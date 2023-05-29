4ca7c2e26fa503aec9ba93f3055177aa966f18db
Judicaelgueni <110699577+Judicaelgueni@users.noreply.github.com>
2023-05-29 23:40:04 +0100

    1
    1

--- script.js
+++ script.js
@@ -2,4 +2,25 @@
 function myonclickFn(){
     alert("Nous sommes vraiment désolé veillez réessaiyer ultérieurement nous ne fonctionnons pas encore. Nous ouvrirons probablement le prochain mois.Pour votre information cette plateforme est faite pour les utilisateur voulant des Délices.")
 }   
+function judi(){
+    var element= document.getElementById("demo");
+    element.style.maxHeight="0px";
+}
+function jano(){
+    var element= document.getElementById("demo");
+    element.style.maxHeight="100px";
+}
+function jap(){
+    document.getElementById("div").innerHTML=("je suis d'accord");
+}
+function log(){
+    document.getElementById("bt");
+    var name= document.getElementById("user").value;
+    var mot= document.getElementById("ok");
+    var jud= 1234;
+    var pass= "Guenai"
+    if(name==jud){(document.location.href="index1.html");
+    alert(bienvenu);
 
+    }else{document.getElementById("moi").innerHTML="Vos identifiants ne sont pas corrects";}
+}
