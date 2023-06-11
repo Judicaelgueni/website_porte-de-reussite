1bbc58204b834bf486df3c8fe599c64b40408226
Judicaelgueni <110699577+Judicaelgueni@users.noreply.github.com>
2023-06-11 20:32:18 +0100

    1
    1

--- script.js
+++ script.js
@@ -8,19 +8,57 @@ function judi(){
 }
 function jano(){
     var element= document.getElementById("demo");
+    var dip= document.getElementById("dep");
+    dip.style.display="block";
     element.style.maxHeight="100px";
 }
 function jap(){
     document.getElementById("div").innerHTML=("je suis d'accord");
 }
 function log(){
-    document.getElementById("bt");
+    
     var name= document.getElementById("user").value;
-    var mot= document.getElementById("ok");
+    var mot= document.getElementById("ok").value;
     var jud= 1234;
-    var pass= "Guenai"
-    if(name==jud){(document.location.href="index1.html");
+    var pass= "Guenai";
+    if(mot==pass && name==jud){(document.location.href="index5.html");
     alert(bienvenu);
 
     }else{document.getElementById("moi").innerHTML="Vos identifiants ne sont pas corrects";}
+
+}
+
+
+function ji(){
+    
+    var a=document.getElementById("auto").setAttribute("src","mach.gif");
+    var b=document.getElementById("self").setAttribute("src","go.gif");
+    if(b){
+        document.getElementById(a);
+    }else{
+        document.getElementById(a);
+    }
+    
+}
+
+
+function maxime(){
+    var noo=document.getElementById("dee").value;
+    document.getElementById("new").innerHTML=noo;
+}
+
+function joo(){
+    var jd=document.getElementById("dee");
+    jd.style.backgroundColor="orange";
+}
+
+function jii(){
+    var jd=document.getElementById("dee");
+    jd.style.backgroundColor="white";
+
+}
+
+function text0(){
+    var t1=document.getElementById("d1");
+    document.getElementById("d1").innerHTML=t1;
 }
